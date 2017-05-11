6bbec28d0d8c103909065aa25054285a7473c5fc
Kirill Volkovich <volkovich@scand.com>
2017-05-11 10:59:06 +0300

    Fixed gh-pages-update.sh

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -1,15 +1,15 @@
-#!/bin/sh 
+#!/bin/sh
 
-rm -rf .gh-pages-tmp build  &&
+rm -rf .gh-pages-tmp build &&
 mkdir .gh-pages-tmp &&
 npm run build-less &&
 npm run npm-build &&
 cp -R build/client/* .gh-pages-tmp &&
-rm -rf .gh-pages-tmp/resources/less  &&
+rm -rf .gh-pages-tmp/resources/less &&
 sed -i -e "s/url('../url('resources/g" .gh-pages-tmp/main.css &&
 sed -i -e "s/src=\"..\//src=\"resources\//g" .gh-pages-tmp/*.html &&
-sed -i -e "s/src=\"..\/..\/img\//src=\"..\/..\/resources\/img\//g" .gh-pages-tmp/demo/html/*.html 
-sed -i -e "s/href=\"..\/..\/\"/href=\"..\/..\/index.html\"/g" .gh-pages-tmp/demo/html/*.html 
+sed -i -e "s/src=\"..\/..\/img\//src=\"..\/..\/resources\/img\//g" .gh-pages-tmp/demo/html/*.html &&
+sed -i -e "s/href=\"..\/..\/\"/href=\"..\/..\/index.html\"/g" .gh-pages-tmp/demo/html/*.html &&
 
 git checkout gh-pages &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
