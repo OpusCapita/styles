23a6fd4c6f125f22315851c8fecd0286ab73c971
Kirill Volkovich <volkovich@scand.com>
2017-05-11 10:47:16 +0300

    Added demo-resources-path.sh

---
+++ demo-resources-path.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sed -i -e "s/url(\"img/url(\"resources\/img/g" main.css &&
+sed -i -e "s/src=\"..\//src=\"resources\//g" dist/*.html &&
+sed -i -e "s/src=\"..\/..\/img\//src=\"..\/..\/resources\/img\//g" dist/demo/html/*.html
+sed -i -e "s/href=\"..\/..\/\"/href=\"..\/..\/index.html\"/g" dist/demo/html/*.html
