7f4e173aeb33a89076765dec715afaeb64def10d
Kirill Volkovich <kvolkovich-sc@users.noreply.github.com>
2017-05-11 17:09:37 +0300

    Update gh-pages-update.sh

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -20,5 +20,5 @@ mv .gh-pages-tmp/* . &&
 rm -rf .gh-pages-tmp &&
 git add -A . &&
 git commit -m "Update gh-pages" &&
-git push --force origin gh-pages &&
+# git push --force origin gh-pages &&
 git checkout master
