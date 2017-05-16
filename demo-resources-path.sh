9cd31fbfa5ece2da56a495a966ee739a945a5481
Sergey Tamkov <tamkov@scand.com>
2017-05-16 16:20:12 +0300

    Fix demo-resource-path.sh

--- demo-resources-path.sh
+++ demo-resources-path.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sed -i -e "s/url(\"img/url(\"resources\/img/url(/g" main.css &&
+sed -i -e "s/url(\"img/url(\"resources\/img/g" dist/main.css &&
+sed -i -e "s/url('../url('resources/g" dist/main.css &&
+
 sed -i -e "s/src=\"..\//src=\"resources\//g" dist/*.html &&
 sed -i -e "s/src=\"..\/..\/img\//src=\"..\/..\/resources\/img\//g" dist/demo/html/*.html
 sed -i -e "s/href=\"..\/..\/\"/href=\"..\/..\/index.html\"/g" dist/demo/html/*.html
