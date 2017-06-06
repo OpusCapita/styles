e69459605533c25ebf6664aaa3fbf170f1b17f4e
Sergey Tamkov <tamkov@scand.com>
2017-06-06 15:37:11 +0300

    Fixed Update demo pages script.

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -15,7 +15,6 @@ git checkout gh-pages &&
 git reset --hard &&
 git pull &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
-find . -type d -empty -delete &&
 mv .gh-pages-tmp/* . &&
 rm -rf .gh-pages-tmp &&
 git add -A . &&
